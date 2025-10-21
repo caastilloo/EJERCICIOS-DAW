b5a67d85be78febcd1367729f5417b73bea41378
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-10-21 14:30:18 +0200

    practica calculadora

--- TEMA2/src/main/java/org/example/PRACTICA2CALCULADORA.java
+++ TEMA2/src/main/java/org/example/PRACTICA2CALCULADORA.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.lang.classfile.attribute.ModuleAttribute;
 import java.util.Scanner;
 
 public class PRACTICA2CALCULADORA {
@@ -8,57 +7,71 @@ public class PRACTICA2CALCULADORA {
     static void main() {
 
         Scanner teclado = new Scanner(System.in);
-        float operando1 = 0;
-        float operando2 = 0;
-        float resultado = 0;
+
+        double operando1 = 0;
+        double operando2 = 0;
+        char operacion = 'x';
+        double resultado = 0;
 
         System.out.println("**** BIENVENIDO A LA CALCULADORA RÁPIDA ****");
 
-        System.out.println("> Introduce operando:");
-        operando1 = teclado.nextFloat();
+        try {
+            System.out.println("> Introduce operando:");
+            operando1 = teclado.nextFloat();
 
-        System.out.println("----------------------------");
+            System.out.println("----------------------------");
 
-        System.out.println("[+] -> sumar");
-        System.out.println("[-] -> restar");
-        System.out.println("[x] -> multiplicar");
-        System.out.println("[/] -> dividir");
-        System.out.println("[R] -> raíz cuadrada");
+            System.out.println("[+] -> sumar");
+            System.out.println("[-] -> restar");
+            System.out.println("[x] -> multiplicar");
+            System.out.println("[/] -> dividir");
+            System.out.println("[R] -> raíz cuadrada");
 
-        System.out.println("----------------------------");
+            System.out.println("----------------------------");
 
-        System.out.println("> Elige una operación:");
-        char operacion = teclado.next().charAt(0);
+            System.out.println("> Elige una operación:");
+            operacion = teclado.next().charAt(0);
 
-        switch (operacion){
-            case '+':
-                float suma = operando1 + operando2;
-                resultado = suma;
-                break;
-            case '-':
-                float resta = operando1 - operando2;
-                resultado = resta;
-                break;
-            case 'x':
-                float multiplicacion = operando1 * operando2;
-                resultado = multiplicacion;
-                break;
-            case '/':
-                float division = operando1 / operando2;
-                resultado = division;
-                break;
-            case 'R':
-                break;
-            default:
-                System.out.println("Introduce una operación válida ...");
-                break;
+            if (operacion!= 'R'){
+                System.out.println("> Introduce el segundo operando:");
+                operando2 = teclado.nextFloat();
+            }
+
+            switch (operacion){
+                case '+':
+                    resultado = operando1+operando2;
+                    break;
+                case '-':
+                    resultado = operando1-operando2;
+                    break;
+                case 'x':
+                    resultado = operando1*operando2;
+                    break;
+                case '/':
+                    resultado = operando1/operando2;
+                    break;
+                case 'R':
+                    resultado = Math.sqrt(operando1);
+                    break;
+                default:
+                    System.out.println("Introduce una operación válida ...");
+                    return;
+            }
+
+            if (operacion=='/' && operando2==0){
+                System.out.println("No se puede dividir entre 0");
+            }else{
+                System.out.println("El resultado de " + operando1 + " " + operacion + " " + operando2 + " es " + resultado);
+            }
+
+        }catch (Exception err){
+            System.out.println("ERROR. Formato incorrecto");
         }
 
 
-        System.out.println("> Introduce el segundo operando:");
-        operando2 = teclado.nextFloat();
 
-        System.out.println("El resultado de " + operando1 + operacion + operando2 + " es " + resultado);
+
+
 
     }
 
