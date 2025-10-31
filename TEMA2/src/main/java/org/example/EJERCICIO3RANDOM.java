01b869f589e9d7cb947da59638cebc608b1d5e72
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-10-31 08:11:47 +0100

    ejercicio 3 random

--- TEMA2/src/main/java/org/example/EJERCICIO3RANDOM.java
+++ TEMA2/src/main/java/org/example/EJERCICIO3RANDOM.java
@@ -10,21 +10,28 @@ public class EJERCICIO3RANDOM {
         Scanner teclado = new Scanner(System.in);
         Random aleatorio = new Random();
 
-        System.out.println("Introduzca el rango máximo:");
-        int max = teclado.nextInt();
+        int max = 0;
+        int min = 0;
 
-        System.out.println("Introduzca el rango mínimo:");
-        int min = teclado.nextInt();
+        do{
+            System.out.println("Introduzca el rango mínimo:");
+            min = teclado.nextInt();
 
-        int verificar = aleatorio.nextInt((max - min + 1) + min);
+            System.out.println("Introduzca el rango máximo:");
+            max = teclado.nextInt();
+
+            if (min >= max){
+                System.out.println("ERROR. EL MINIMO TIENE QUE SER MENOR QUE LE MAXIMO.");
+            }
+
+        }while (min>=max);
 
         System.out.println("Cuantos numeros aleatorios quieres:");
         int numero_aleatorios = teclado.nextInt();
 
         for (int i = 0; i<numero_aleatorios; i++){
-            int num = aleatorio.nextInt(max)+1;
+            int num = aleatorio.nextInt(max - min + 1) + min;
             System.out.println(num);
-
         }
 
     }
