b923417512d947f41eb3de6e3a7f2f3eb1d234ba
A606\Jose <joracaam2006@gmail.com>
2025-11-13 17:16:55 +0100

    eliminar duplcados y ordenar array (diferentes formas)

---
+++ TEMA 3/TEMA 3/src/main/java/org/example/EliminarDuplicados2.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class EliminarDuplicados2 {
+
+    static void main() {
+
+        
+
+
+    }
+}
--- TEMA 3/TEMA 3/src/main/java/org/example/Ordenar.java
+++ TEMA 3/TEMA 3/src/main/java/org/example/Ordenar.java
@@ -1,6 +1,8 @@
 package org.example;
 
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 public class Ordenar {
 
@@ -8,8 +10,17 @@ public class Ordenar {
 
         int vector [] = {7,5,10,2,1};
 
-        Arrays.sort(vector);
+        int vector_copia [] = vector.clone();
+
+        Arrays.sort(vector_copia);
         System.out.println(Arrays.toString(vector));
+        System.out.println(Arrays.toString(vector_copia));
+
+//        Arrays.sort(vector_copia, Collections.reverseOrder()); //Para utilizarlo hace falta pasar a Integer
+//        System.out.println(Arrays.toString(vector_copia));
+
+        int posicion = Arrays.binarySearch(vector_copia, 10);
+        System.out.println(posicion);
 
     }
 
