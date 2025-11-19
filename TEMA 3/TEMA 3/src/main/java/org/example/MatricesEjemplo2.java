3a8595ad179043f89d0184f052f7dab2f5e02316
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-11-19 11:23:57 +0100

    ejercicio matrices 2

--- TEMA 3/TEMA 3/src/main/java/org/example/MatricesEjemplo2.java
+++ TEMA 3/TEMA 3/src/main/java/org/example/MatricesEjemplo2.java
@@ -4,10 +4,28 @@ public class MatricesEjemplo2 {
 
     static void main() {
 
+        int matrizA [][] = {{1,2,3},{6,7,8},{10,8,15}};
+        int matrizB [][] = {{5,1,4},{7,3,11},{11,12,13}};
 
+        int matrizM [][] = new int[3][3];
 
+        for (int i = 0; i < matrizA.length; i++) {
 
+            for (int j = 0; j < matrizA[i].length; j++) {
 
+                int numA = matrizA[i][j];
+                int numB = matrizB[i][j];
+
+                if (numA>numB){
+                    System.out.print(matrizA[i][j] + " ");
+                }else {
+                    System.out.print(matrizB[i][j] + " ");
+                }
+
+            }
+
+            System.out.println();
+        }
     }
 
 }
