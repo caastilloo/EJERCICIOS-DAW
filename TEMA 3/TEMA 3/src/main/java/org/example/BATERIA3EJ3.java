cd929b146c008a0b967b9a817f27ef8959dd810a
A606\Jose <joracaam2006@gmail.com>
2025-11-20 20:12:41 +0100

    bateria 3 ejercicio 3

---
+++ TEMA 3/TEMA 3/src/main/java/org/example/BATERIA3EJ3.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class BATERIA3EJ3 {
+
+    static void main() {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println("Hola! Cuántos estudiantes tienes?");
+        int numero_estudiantes = teclado.nextInt();
+
+        System.out.println("Cuántas asignaturas tienes?");
+        int numero_asignaturas = teclado.nextInt();
+
+
+    }
+}
