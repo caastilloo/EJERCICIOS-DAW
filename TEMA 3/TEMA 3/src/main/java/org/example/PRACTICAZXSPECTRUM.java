d7b281df47573e2455cb73d2901858478663a7c7
A606\Jose <joracaam2006@gmail.com>
2025-11-21 19:14:40 +0100

    version 1

--- TEMA 3/TEMA 3/src/main/java/org/example/PRACTICAZXSPECTRUM.java
+++ TEMA 3/TEMA 3/src/main/java/org/example/PRACTICAZXSPECTRUM.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PRACTICAZXSPECTRUM {
@@ -18,7 +19,29 @@ public class PRACTICAZXSPECTRUM {
         System.out.println("Alto");
         int alto = teclado.nextInt();
 
+        String matriz [][] = new String[ancho][alto];
+
         System.out.println("Introduce (línea a línea) los colores de tu imagen para cada píxel:");
+
+        for (int i = 0; i < matriz.length; i++) {
+
+            String color[] = teclado.next().split("");
+
+            if (color.length==matriz[i].length){
+                for (int j = 0; j < matriz[i].length; j++) {
+
+                    matriz[i][j] = color[j];
+
+                }
+            }else {
+                System.out.println("El tamaño es incorrecto...");
+                i--;
+            }
+        }
+
+        for (String fila[] : matriz){
+            System.out.println(fila);
+        }
 
     }
 }
