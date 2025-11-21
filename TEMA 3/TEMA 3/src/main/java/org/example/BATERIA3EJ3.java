a8bb32a6fa161c8183c8d4fb0bad652d852baef8
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-11-21 13:49:48 +0100

    version 1 ej3 bateria3

--- TEMA 3/TEMA 3/src/main/java/org/example/BATERIA3EJ3.java
+++ TEMA 3/TEMA 3/src/main/java/org/example/BATERIA3EJ3.java
@@ -14,6 +14,31 @@ public class BATERIA3EJ3 {
         System.out.println("Cuántas asignaturas tienes?");
         int numero_asignaturas = teclado.nextInt();
 
+        String matriz [][] = new String[numero_estudiantes+1][numero_asignaturas+1];
+
+
+
+//        do {
+//            System.out.println("Introduce el nombre del estudiante " + i);
+//            matriz[i][j] = teclado.next();
+//        }while (i<numero_estudiantes);
+
+
+//        for (int i = 0; i < numero_estudiantes; i++) {
+//
+//            System.out.println("Introduce el nombre del estudiante " + i);
+//
+//        }
+//        for (int i = 0; i < matriz.length; i++) {
+//
+//
+//            for (int j = 0; j < matriz[i].length; j++) {
+//
+//
+//
+//            }
+//
+//        }
 
     }
 }
