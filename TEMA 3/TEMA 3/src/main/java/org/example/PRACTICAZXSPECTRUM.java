c7f7528b929910dd3e5c299b9a53d600c5d479ca
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-11-22 23:10:16 +0100

    version 2

--- TEMA 3/TEMA 3/src/main/java/org/example/PRACTICAZXSPECTRUM.java
+++ TEMA 3/TEMA 3/src/main/java/org/example/PRACTICAZXSPECTRUM.java
@@ -16,10 +16,10 @@ public class PRACTICAZXSPECTRUM {
         System.out.println("Ancho:");
         int ancho = teclado.nextInt();
 
-        System.out.println("Alto");
+        System.out.println("Alto:");
         int alto = teclado.nextInt();
 
-        String matriz [][] = new String[ancho][alto];
+        String matriz [][] = new String[alto][ancho];
 
         System.out.println("Introduce (línea a línea) los colores de tu imagen para cada píxel:");
 
@@ -40,7 +40,42 @@ public class PRACTICAZXSPECTRUM {
         }
 
         for (String fila[] : matriz){
-            System.out.println(fila);
+            System.out.println(Arrays.toString(fila));
+        }
+        
+        String validador_bloque1 = "";
+        int contador = 1;
+        // FILAS
+        for (int i = 0; i < matriz.length; i++) {
+
+            validador_bloque1 = matriz[0][0];
+            // COLUMNAS
+            for (int j = 0; j < 8; j++) {
+                
+                if (matriz[i][j]!=validador_bloque1){
+                    contador++;
+                }
+
+            }
+        }
+
+        int contador2 = 1;
+        String validador_bloque2 = "";
+        for (int i = 0; i < matriz.length; i++) {
+
+            for (int j = 8; j < 16; j++) {
+
+                validador_bloque2 = matriz[0][8];
+                if (matriz[i][j]!=validador_bloque2){
+                    contador2++;
+                }
+            }
+        }
+
+        if (contador<=2 && contador2<=2){
+            System.out.println("Es compatible con ZX Spectrum");
+        }else {
+            System.out.println("No es compatible con ZX Spectrum");
         }
 
     }
