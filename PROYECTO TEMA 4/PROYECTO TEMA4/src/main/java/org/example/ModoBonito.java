102387ddc2b820be20ba247090a1f18bd63cfe1f
DESKTOP-OV7P4OJ\jorac <josercaastilloo@gmail.com>
2025-12-17 14:42:24 +0100

    version 1 (revisar)

--- PROYECTO TEMA 4/PROYECTO TEMA4/src/main/java/org/example/ModoBonito.java
+++ PROYECTO TEMA 4/PROYECTO TEMA4/src/main/java/org/example/ModoBonito.java
@@ -1,14 +1,150 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class ModoBonito {
 
     static void main() {
 
-        System.out.println("*** BIENVENIDO A LA VALIDACIÓN DE MATRICES ***");
-
-
+        introduccion();
+        ejemplos();
+        ejecutar();
 
     }
 
+    public static void introduccion(){
+        System.out.println("=============================================");
+        System.out.println("      PROGRAMA DE MATRICES TRIANGULARES    ");
+        System.out.println("=============================================");
+        System.out.println();
+        System.out.println("Este programa comprueba si una matriz");
+        System.out.println("cuadrada es triangular superior o inferior.");
+        System.out.println();
+    }
 
+    public static void ejemplos(){
+        System.out.println("----------------- EJEMPLOS -------------------");
+
+        System.out.println("Ejemplo de matriz triangular SUPERIOR:");
+        int superior [][] = {
+                {1, 0, 0},
+                {4, 5, 0},
+                {7, 8, 9}
+        };
+
+        for (int sup[] : superior){
+            System.out.println(Arrays.toString(sup));
+        }
+
+        System.out.println("Ejemplo de matriz triangular INFERIOR:");
+        int inferior [][] = {
+                {1, 2, 3},
+                {0, 5, 6},
+                {0, 0, 9}
+        };
+
+        for (int inf[] : inferior){
+            System.out.println(Arrays.toString(inf));
+        }
+
+        System.out.println("----------------------------------------------");
+    }
+
+    public static void menu(){
+        System.out.println();
+
+        System.out.println("------------------- MENU ---------------------");
+        System.out.println("[1] - Comprobar una matriz");
+        System.out.println("[2] - Salir");
+        System.out.print("-> Elige un opción: ");
+    }
+
+    public static void ejecutar(){
+
+        Scanner teclado = new Scanner(System.in);
+        boolean salir = false;
+
+        while (!salir){
+
+            menu();
+            char opcion = teclado.next().toUpperCase().charAt(0);
+
+            switch (opcion){
+                case '1':
+                    procesarMatriz(teclado);
+                    break;
+                case '2':
+                    System.out.println("Saliendo del programa ...");
+                    salir = true;
+                    break;
+                default:
+                    System.out.println("ERROR. Introduce una opción válida.");
+            }
+        }
+
+    }
+
+    public static int[][] leerMatriz (Scanner teclado, int n){
+
+        int matriz [][] = new int[n][n];
+
+        System.out.println("Introduce los valores de la matriz:");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matriz[i][j] = teclado.nextInt();
+            }
+        }
+
+        System.out.println("----------------------------------------------");
+        System.out.println("Tu matriz:");
+        for (int matr[] : matriz){
+            System.out.println(Arrays.toString(matr));
+        }
+
+        return matriz;
+    }
+
+    public static void procesarMatriz(Scanner teclado){
+        System.out.println("----------------------------------------------");
+
+        System.out.println("Introduce el tamaño de la matriz: ");
+        int n = teclado.nextInt();
+
+        int matriz [][] = leerMatriz(teclado, n);
+
+        boolean resultado = comprobarTriangular(matriz);
+
+        System.out.println();
+
+        if (resultado){
+            System.out.println("-> La matriz ES triangular.");
+        }else {
+            System.out.println("-> La matriz NO es triangular.");
+        }
+        System.out.println("----------------------------------------------");
+    }
+
+    public static boolean comprobarTriangular (int [][] matriz){
+
+        boolean superior = true;
+        boolean inferior = true;
+        int n = matriz.length;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if (i > j && matriz[i][j] != 0) {
+                    superior = false;
+                }
+
+                if (i < j && matriz[i][j] != 0){
+                    inferior = false;
+                }
+            }
+
+        }
+
+        return superior || inferior;
+    }
 }
---
+++ Proyecto ProgramaME/Matrices Triangulares/src/main/java/org/example/ModoProgramaME.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class ModoProgramaME {
+
+    static Scanner teclado = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        teclado = new java.util.Scanner(System.in);
+
+        while(casoDePrueba())
+            ;
+
+    } // main
+
+    public static boolean casoDePrueba() {
+
+        // LEER CASO DE PRUEBA
+
+
+        if (//es el caso de prueba que marca el final)
+        return false;
+    else {
+            // CÓDIGO PRINCIPAL AQUÍ
+            // Procesa un único caso leyendo con
+            // in.next*()
+
+            return true;
+        }
+
+    } // casoDePrueba
+
+}
