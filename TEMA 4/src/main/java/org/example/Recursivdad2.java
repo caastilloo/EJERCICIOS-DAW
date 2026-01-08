2ae9006ae2f96370cc976e08147a92937d40f4c8
A606\Jose <joracaam2006@gmail.com>
2026-01-08 17:31:23 +0100

    recursividad

---
+++ TEMA 4/src/main/java/org/example/Recursivdad2.java
@@ -0,0 +1,21 @@
+package org.example;
+
+public class Recursivdad2 {
+
+    static void main() {
+
+        int num = 1;
+        System.out.println(suma(10));
+
+    }
+
+    public static int suma(int num){
+
+        if (num==0){
+            return 0;
+        }else {
+            return num + suma(num-1);
+        }
+
+    }
+}
