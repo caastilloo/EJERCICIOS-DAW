433a54b467e62e8f949d4158c90d944789a67ef3
Jose <josercaastilloo@gmail.com>
2026-02-17 21:11:01 +0100

    ++

---
+++ TEMA 6/TEMA 6/src/main/java/PruebasEnum/MetodosApp.java
@@ -0,0 +1,28 @@
+package PruebasEnum;
+
+import SistemaPagoEcommerce.MetodoPago;
+
+public class MetodosApp {
+
+    static void main() {
+
+        Metodo opcion = Metodo.PAYPAL;
+
+        System.out.println("Opción: " + opcion);
+
+        System.out.println();
+
+        System.out.println("MÉTODOS VÁLIDOS:");
+        for (Metodo metodo : Metodo.values()){
+            System.out.println(metodo);
+        }
+
+        System.out.println();
+
+        System.out.println("Datos METODO: ");
+        System.out.println("Posición: " + opcion.ordinal());
+        System.out.println("Nombre: " + opcion.name());
+        System.out.println("Comisión: " + opcion.getComision());
+        
+    }
+}
