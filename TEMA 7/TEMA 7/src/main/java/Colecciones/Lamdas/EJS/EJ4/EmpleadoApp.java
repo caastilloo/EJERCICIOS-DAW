4f15a68d108fe7cbb4e147595dc3f9bf0a2ded11
DESKTOP-2AO1MTI\Jose <josercaastilloo@gmail.com>
2026-03-27 16:59:03 +0100

    ++

---
+++ TEMA 7/TEMA 7/src/main/java/Colecciones/Lamdas/EJS/EJ4/EmpleadoApp.java
@@ -0,0 +1,20 @@
+package Colecciones.Lamdas.EJS.EJ4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmpleadoApp {
+
+    static List<Empleado> listaEmpleados = new ArrayList<>();
+
+    static void main() {
+
+        Empleado jose = new Empleado("Jose", 2.500, "20/07/2006");
+        Empleado pablo = new Empleado("Pablo", 2.000, "15/04/2000");
+        Empleado alvaro = new Empleado("Alvaro", 1.800, "02/01/2006");
+        Empleado pascual = new Empleado("Pascual", 2.800, "03/06/2006");
+
+
+
+    }
+}
